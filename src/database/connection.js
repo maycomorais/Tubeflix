572dbdf8035d2c6ed8ae3dc5da4974b253e0cc5a
Maycon Morais <97128625+maycomorais@users.noreply.github.com>
2022-05-09 17:45:23 -0300

    atualização

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,16 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-console.log(process.env.DB_USER);
+// console.log(process.env.DB_USER);
 
 export const connection = new Sequelize(
-    process.env.DB_BASE, //Database
-    process.env.DB_USER, //Usuário
-    process.env.DB_PASS, //Senha
-    {
-        host: process.env.DB_LOCAL, //URL banco de Dados
-        port: 5432, //Porta que roda o banco de Dados
-        dialect: 'postgres' //Qual banco de dados o sequelize está trabalahando
+    process.env.DB_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
+    // process.env.DB_BASE, //Database
+    // process.env.DB_USER, //Usuário
+    // process.env.DB_PASS, //Senha
+    // {
+    //     host: process.env.DB_LOCAL, //URL banco de Dados
+    //     port: 5432, //Porta que roda o banco de Dados
+    //     dialect: 'postgres' //Qual banco de dados o sequelize está trabalahando
+    // }
 );
 
